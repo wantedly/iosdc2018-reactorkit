679e81346c4d747e024c117d4e29dddf8101cbfa
Masatoshi Kubode <masatoshi@wantedly.com>
2018-08-09 16:40:53 +0900

    Sort files

--- iosdc2018-livecoding/ViewController.swift
+++ iosdc2018-livecoding/ViewController.swift
@@ -1,15 +1,3 @@
-/*
-User stories:
-
-- [x] Able to enter texts up to 140 chars
-- [x] Able to post
-- [x] Able to know what is a number of rest chars we can write
-- [x] Able to know we can post by disabled post button
-- [x] Able to know we are posting now
-- [x] Able to know tweeting is complete
-- [x] Able to know the error of duplicated content to tweet
-*/
-
 import UIKit
 
 class ViewController: UINavigationController {
