715e101acb1c4430ab3661b7d11f60d1369c9780
Masatoshi Kubode <masatoshi@wantedly.com>
2018-08-09 16:36:51 +0900

    Split to files

---
+++ iosdc2018-livecoding/TweetViewController.swift
@@ -0,0 +1,99 @@
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+import Then
+import UITextView_Placeholder
+import SnapKit
+
+class TweetViewController: UIViewController, View {
+    private let tweetButton = UIBarButtonItem(title: "Tweet", style: UIBarButtonItemStyle.done, target: nil, action: nil)
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+        $0.isLayoutMarginsRelativeArrangement = true
+    }
+    private let textView = UITextView().then {
+        $0.placeholder = "いまどうしてる？"
+    }
+    private let remainsLabel = UILabel().then {
+        $0.textAlignment = .right
+    }
+    private let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray).then {
+        $0.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+    }
+
+    var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = tweetButton
+        navigationController?.navigationBar.backgroundColor = .white
+        view.addSubview(stackView.then {
+            $0.addArrangedSubview(textView)
+            $0.addArrangedSubview(remainsLabel)
+        })
+        view.addSubview(activityIndicator)
+        activityIndicator.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    func bind(reactor: TweetReactor) {
+        // Action
+        tweetButton.rx.tap
+            .map { Reactor.Action.tweet }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        textView.rx.text.orEmpty
+            .map { Reactor.Action.updateText($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        // State
+        reactor.state
+            .map { $0.tweeting }
+            .distinctUntilChanged()
+            .bind(to: activityIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map { $0.tweetButtonEnabled }
+            .distinctUntilChanged()
+            .bind(to: tweetButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map { "残り: \($0.numberOfTextCountRemains)文字" }
+            .distinctUntilChanged()
+            .bind(to: remainsLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        // Relay
+        reactor.completedRelay
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                let alert = UIAlertController(title: nil, message: "Tweet successfully", preferredStyle: .alert).then {
+                    $0.addAction(UIAlertAction(title: "OK", style: .default))
+                }
+                self?.present(alert, animated: true)
+            })
+            .disposed(by: disposeBag)
+
+        reactor.errorRelay
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                let alert = UIAlertController(title: nil, message: $0.localizedDescription, preferredStyle: .alert).then {
+                    $0.addAction(UIAlertAction(title: "OK", style: .default))
+                }
+                self?.present(alert, animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+}
---
+++ iosdc2018-livecoding/TwitterService.swift
@@ -0,0 +1,27 @@
+import RxSwift
+
+enum TweetError: LocalizedError {
+    case duplicated
+    var errorDescription: String? {
+        switch self {
+        case .duplicated:
+            return "Your tweet is duplicated with the last one."
+        }
+    }
+}
+
+class TwitterService {
+    static let MAX_TEXT_LENGTH = 140
+
+    private let scheduler = SerialDispatchQueueScheduler(internalSerialQueueName: "tweet")
+
+    private var lastTweetedText: String? = nil
+
+    func tweet(text: String) -> Single<Bool> {
+        if lastTweetedText == text {
+            return Single.just(false).delay(1, scheduler: scheduler).flatMap { _ in Single.error(TweetError.duplicated) }
+        }
+        lastTweetedText = text
+        return Single.just(true).delay(1, scheduler: scheduler)
+    }
+}
