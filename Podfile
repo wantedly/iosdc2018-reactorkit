e0c411592eda2a299d13f7009fc2000c3e878a57
Masatoshi Kubode <masatoshi@wantedly.com>
2018-08-09 15:22:41 +0900

    Layout

--- Podfile
+++ Podfile
@@ -7,7 +7,15 @@ target 'iosdc2018-livecoding' do
   pod 'RxCocoa'
   pod 'RxSwift'
   pod 'Then'
+  pod 'UITextView+Placeholder'
   pod 'ReactorKit'
   pod 'Reveal-SDK', :configurations => ['Debug']
   pod 'SnapKit'
+end
+
+post_install do |installer| # Fix for: https://github.com/wantedly/visit-ios/issues/14
+    podsTargets = installer.pods_project.targets.find_all { |target| target.name.start_with?('Pods') }
+    podsTargets.each do |target|
+        target.frameworks_build_phase.clear
+    end
 end
--- iosdc2018-livecoding/ViewController.swift
+++ iosdc2018-livecoding/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import Then
+import SnapKit
+import UITextView_Placeholder
 
-class ViewController: UIViewController {
+class ViewController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [TweetViewController()]
+    }
+}
+
+class TweetViewController: UIViewController {
+    private let tweetButton = UIBarButtonItem(title: "Tweet", style: UIBarButtonItemStyle.done, target: nil, action: nil)
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+        $0.isLayoutMarginsRelativeArrangement = true
+    }
+    private let textView = UITextView().then {
+        $0.placeholder = "いまどうしてる？"
+    }
+    private let remainsLabel = UILabel().then {
+        $0.text = "10/140"
+        $0.textAlignment = .right
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = tweetButton
+        navigationController?.navigationBar.backgroundColor = .white
+        view.addSubview(stackView.then {
+            $0.addArrangedSubview(textView)
+            $0.addArrangedSubview(remainsLabel)
+        })
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
