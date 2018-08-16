cdb43a1cd1886b4f739d2a64683be26b3215b0ba
Masatoshi Kubode <masatoshi@wantedly.com>
2018-08-17 08:27:44 +0900

    Initial code

--- iosdc2018-livecoding/TweetViewController.swift
+++ iosdc2018-livecoding/TweetViewController.swift
@@ -1,13 +1,13 @@
 /*
  User stories:
 
- - [x] Able to enter texts up to 140 chars
- - [x] Able to post
- - [x] Able to know what is a number of rest chars we can write
- - [x] Able to know we can post by disabled post button
- - [x] Able to know we are posting now
- - [x] Able to know tweeting is complete
- - [x] Able to know the error of duplicated content to tweet
+ - [ ] Able to enter texts up to 140 chars
+ - [ ] Able to post
+ - [ ] Able to know what is a number of rest chars we can write
+ - [ ] Able to know we can post by disabled post button
+ - [ ] Able to know we are posting now
+ - [ ] Able to know tweeting is complete
+ - [ ] Able to know the error of duplicated content to tweet
  */
 
 import UIKit
@@ -18,8 +18,9 @@ import Then
 import UITextView_Placeholder
 import SnapKit
 
-class TweetViewController: UIViewController, View {
-    private let tweetButton = UIBarButtonItem(title: "Tweet", style: UIBarButtonItemStyle.done, target: nil, action: nil)
+class TweetViewController: UIViewController {
+
+    private let tweetButton = UIBarButtonItem(title: "Tweet", style: .done, target: nil, action: nil)
     private let stackView = UIStackView().then {
         $0.axis = .vertical
         $0.layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
@@ -40,72 +41,24 @@ class TweetViewController: UIViewController, View {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Setup
         view.backgroundColor = .white
         navigationItem.rightBarButtonItem = tweetButton
         navigationController?.navigationBar.backgroundColor = .white
+
+        // Hierarchy
         view.addSubview(stackView.then {
             $0.addArrangedSubview(textView)
             $0.addArrangedSubview(remainsLabel)
         })
         view.addSubview(activityIndicator)
+
+        // Constraints
         activityIndicator.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         stackView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
         }
-    }
-
-    func bind(reactor: TweetReactor) {
-        // Action
-        tweetButton.rx.tap
-            .map { Reactor.Action.tweet }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-
-        textView.rx.text.orEmpty
-            .map { Reactor.Action.updateText($0) }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-
-        // State
-        reactor.state
-            .map { $0.tweeting }
-            .distinctUntilChanged()
-            .bind(to: activityIndicator.rx.isAnimating)
-            .disposed(by: disposeBag)
-
-        reactor.state
-            .map { $0.tweetButtonEnabled }
-            .distinctUntilChanged()
-            .bind(to: tweetButton.rx.isEnabled)
-            .disposed(by: disposeBag)
-
-        reactor.state
-            .map { "残り: \($0.numberOfTextCountRemains)文字" }
-            .distinctUntilChanged()
-            .bind(to: remainsLabel.rx.text)
-            .disposed(by: disposeBag)
-
-        // Relay
-        reactor.completedRelay
-            .observeOn(MainScheduler.instance)
-            .subscribe(onNext: { [weak self] in
-                let alert = UIAlertController(title: nil, message: "Tweet successfully", preferredStyle: .alert).then {
-                    $0.addAction(UIAlertAction(title: "OK", style: .default))
-                }
-                self?.present(alert, animated: true)
-            })
-            .disposed(by: disposeBag)
-
-        reactor.errorRelay
-            .observeOn(MainScheduler.instance)
-            .subscribe(onNext: { [weak self] in
-                let alert = UIAlertController(title: nil, message: $0.localizedDescription, preferredStyle: .alert).then {
-                    $0.addAction(UIAlertAction(title: "OK", style: .default))
-                }
-                self?.present(alert, animated: true)
-            })
-            .disposed(by: disposeBag)
     }
 }
--- iosdc2018-livecoding/ViewController.swift
+++ iosdc2018-livecoding/ViewController.swift
@@ -4,9 +4,7 @@ class ViewController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewControllers = [
-            TweetViewController().then {
-                $0.reactor = TweetReactor()
-            }
+            TweetViewController()
         ]
     }
 }
